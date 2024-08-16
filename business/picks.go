3201e0d1dd81cf896bf9979c382af49592bcf0aa
frisk038 <frisktrentehuit@gmail.com>
2024-08-16 17:06:24 +0200

    at least two voyelle

--- business/picks.go
+++ business/picks.go
@@ -20,13 +20,15 @@ type BPicks struct {
 	store pickStore
 }
 
-var alphabet = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-	"k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-	"u", "v", "w", "x", "y", "z"}
+var consonne = []string{"b", "c", "d", "f", "g", "h", "j",
+	"k", "l", "m", "n", "p", "q", "r", "s", "t",
+	"v", "w", "x", "z"}
 
-var values = map[string]int{"a": 1, "b": 3, "c": 3, "d": 2, "e": 1, "f": 4, "g": 2, "h": 4, "i": 1, "j": 8,
-	"k": 5, "l": 1, "m": 3, "n": 1, "o": 1, "p": 3, "q": 10, "r": 1, "s": 1,
-	"t": 1, "u": 1, "v": 4, "w": 4, "x": 8, "y": 4, "z": 10}
+var voyelle = []string{"a", "e", "i", "o", "u", "y"}
+
+var values = map[string]int{"a": 1, "b": 3, "c": 3, "d": 2, "e": 1, "f": 4,
+	"g": 2, "h": 4, "i": 1, "j": 8, "k": 5, "l": 1, "m": 3, "n": 1, "o": 1, "p": 3,
+	"q": 10, "r": 1, "s": 1, "t": 1, "u": 1, "v": 4, "w": 4, "x": 8, "y": 4, "z": 10}
 
 func NewBPicks(store pickStore) BPicks {
 	return BPicks{store: store}
@@ -36,8 +38,12 @@ func (bp *BPicks) GetDailyWord(ctx context.Context) (models.Pick, error) {
 	p, err := bp.store.GetDailyWord(ctx)
 	if err != nil {
 		if err == models.ErrNoDailyPick {
-			rand.Shuffle(26, func(i, j int) { alphabet[i], alphabet[j] = alphabet[j], alphabet[i] })
-			p.Letters = alphabet[:9]
+			rand.Shuffle(len(voyelle), func(i, j int) { voyelle[i], voyelle[j] = voyelle[j], voyelle[i] })
+			rand.Shuffle(len(consonne), func(i, j int) { consonne[i], consonne[j] = consonne[j], consonne[i] })
+
+			p.Letters = consonne[:7]
+			p.Letters = append(p.Letters, voyelle[:2]...)
+			rand.Shuffle(len(p.Letters), func(i, j int) { p.Letters[i], p.Letters[j] = p.Letters[j], p.Letters[i] })
 			p.ID, err = bp.store.InsertLetters(ctx, p.Letters)
 			if err != nil {
 				return models.Pick{}, err
